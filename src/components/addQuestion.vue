3e7a041dfecef31a2fda2c4e77396929380b39be
肌肉仔 <itzhengwei163.com>
2020-07-22 19:39:18 +0800

    修改bug完成

--- src/components/addQuestion.vue
+++ src/components/addQuestion.vue
@@ -1,13 +1,7 @@
 <template>
   <el-dialog class="addQuestion" :visible.sync="isShow" :fullscreen="true">
     <div slot="title" class="title">新增题库测试</div>
-    <el-form
-      ref="form"
-      class="form"
-      :rules="rules"
-      :model="form"
-      label-width="100px"
-    >
+    <el-form ref="form" class="form" :rules="rules" :model="form" label-width="100px">
       <el-form-item label="学科" prop="subject">
         <el-select v-model="form.subject" placeholder="请选择学科">
           <el-option
@@ -37,20 +31,11 @@
       </el-form-item>
       <el-form-item label="城市" prop="city">
         <!-- cascader级联选择器  options是数据  props是配置 把label的值当作value -->
-        <el-cascader
-          v-model="form.city"
-          :options="options"
-          :props="{ value: 'label' }"
-        ></el-cascader>
+        <el-cascader v-model="form.city" :options="options" :props="{ value: 'label' }"></el-cascader>
       </el-form-item>
       <el-form-item label="题型" prop="type">
         <el-radio-group v-model="form.type">
-          <el-radio
-            :label="index + 1"
-            v-for="(item, index) in typeArr"
-            :key="index"
-            >{{ item }}</el-radio
-          >
+          <el-radio :label="index + 1" v-for="(item, index) in typeArr" :key="index">{{ item }}</el-radio>
         </el-radio-group>
       </el-form-item>
 
@@ -75,7 +60,7 @@
       <hr />
       <el-form-item label="解析视频">
         <!-- <el-button type="primary">点击上传</el-button> -->
-        <upload id="hahah" v-model="form.video" type="video"> </upload>
+        <upload id="hahah" v-model="form.video" type="video"></upload>
       </el-form-item>
       <hr />
       <el-form-item label="答案解析" prop="answer_analyze">
@@ -99,49 +84,98 @@
 
 <script>
 //导入接口
-import { add } from "@/api/question/question.js"
+import { add } from "@/api/question/question.js";
 //导入上传组件
-import upload from "./upload"
+import upload from "./upload";
 //导入单选组件
-import allSelect from "./allSelect"
+import allSelect from "./allSelect";
 //导入省市区数
-import { regionData } from "element-china-area-data"
+import { regionData } from "element-china-area-data";
 //导入富文本编辑器
-import "quill/dist/quill.core.css"
-import "quill/dist/quill.snow.css"
-import "quill/dist/quill.bubble.css"
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
 
-import { quillEditor } from "vue-quill-editor"
+import { quillEditor } from "vue-quill-editor";
 export default {
+  //侦听器
+  watch: {
+    isShow(newVal) {
+      if (newVal == false) {
+        //初始化
+        this.form = {
+          title: "", //	是	string	标题
+          subject: "", //	是	int	学科id标识
+          step: "", //	是	int	阶段1、初级 2、中级 3、高级
+          enterprise: "", //	是	int	企业id标识
+          city: [], //	是	array	[省、市、区县]
+          type: 1, //	是	string	题型 1单选 、2多选 、3简答
+          difficulty: 1, //	是	int	题目难度 1简单 、2一般 、3困难
+          single_select_answer: "", //	是	string	单选题答案
+          multiple_select_answer: [], //	是	array	多选题答案
+          short_answer: "", //	是	string	简答题答案
+          video: "", //	否	string	解析视频地址
+          answer_analyze: "", //	是	string	答案解析
+          remark: "", //	是	string	答案备注
+          select_options: [
+            {
+              label: "A",
+              text: "",
+              image: ""
+            },
+            {
+              label: "B",
+              text: "",
+              image: ""
+            },
+            {
+              label: "C",
+              text: "",
+              image: ""
+            },
+            {
+              label: "D",
+              text: "",
+              image: ""
+            }
+          ] //	是	array	选项，介绍，图片介绍
+        };
+        //清除表单验证
+        this.$nextTick(() => {
+          this.$refs.form.clearValidate();
+        });
+      }
+    }
+  },
   //注册
   components: {
     quillEditor,
     allSelect,
-    upload,
+    upload
   },
   props: ["typeArr"],
   methods: {
     //提交功能
     submit() {
-      this.$refs.form.validate((v) => {
+      this.$refs.form.validate(v => {
         if (v) {
           add(this.form).then(() => {
             //提示用户
-            this.$message.success("新增成功")
+            this.$message.success("新增成功");
 
             //刷新数据
-            this.$emit("search")
+            this.$emit("search");
             //关闭对话框
-            this.isShow = false
-          })
+            this.isShow = false;
+          });
         } else {
-          this.$message.error("验证失败")
+          this.$message.error("验证失败");
         }
-      })
+      });
     },
     validateField(aaa) {
-      this.$refs.form.validateField([aaa])
-    },
+      this.$refs.form.validateField([aaa]);
+    }
   },
   data() {
     return {
@@ -150,7 +184,7 @@ export default {
       obj: {
         1: "single_select_answer",
         2: "multiple_select_answer",
-        3: "short_answer",
+        3: "short_answer"
       },
       form: {
         title: "", //	是	string	标题
@@ -169,25 +203,25 @@ export default {
         select_options: [
           {
             label: "A",
-            text: "狗不理",
-            image: "",
+            text: "",
+            image: ""
           },
           {
             label: "B",
-            text: "猫不理",
-            image: "",
+            text: "",
+            image: ""
           },
           {
             label: "C",
-            text: "麻花",
-            image: "",
+            text: "",
+            image: ""
           },
           {
             label: "D",
-            text: "炸酱面",
-            image: "",
-          },
-        ], //	是	array	选项，介绍，图片介绍
+            text: "",
+            image: ""
+          }
+        ] //	是	array	选项，介绍，图片介绍
       },
       rules: {
         title: [{ required: true, message: "必填", trigger: "change" }], //	是	string	标题
@@ -198,21 +232,21 @@ export default {
         type: [{ required: true, message: "必填", trigger: "change" }], //	是	string	题型 1单选 、2多选 、3简答
         difficulty: [{ required: true, message: "必填", trigger: "change" }], //	是	int	题目难度 1简单 、2一般 、3困难
         single_select_answer: [
-          { required: true, message: "必填", trigger: "change" },
+          { required: true, message: "必填", trigger: "change" }
         ], //	是	string	单选题答案
         multiple_select_answer: [
-          { required: true, message: "必填", trigger: "change" },
+          { required: true, message: "必填", trigger: "change" }
         ], //	是	array	多选题答案
         short_answer: [{ required: true, message: "必填", trigger: "change" }], //	是	string	简答题答案
         video: "", //	否	string	解析视频地址
         answer_analyze: [
-          { required: true, message: "必填", trigger: "change" },
+          { required: true, message: "必填", trigger: "change" }
         ], //	是	string	答案解析
-        remark: [{ required: true, message: "必填", trigger: "change" }], //	是	string	答案备注
-      },
-    }
-  },
-}
+        remark: [{ required: true, message: "必填", trigger: "change" }] //	是	string	答案备注
+      }
+    };
+  }
+};
 </script>
 
 <style lang="less">
--- src/views/layout/children/questionList.vue
+++ src/views/layout/children/questionList.vue
@@ -56,7 +56,7 @@
           <el-col :span="6">
             <el-form-item label="难度" prop="difficulty">
               <el-select v-model="form.difficulty">
-                <el-option label="简单" :value="0"></el-option>
+                <el-option label="简单" :value="1"></el-option>
                 <el-option label="一般" :value="2"></el-option>
                 <el-option label="困难" :value="3"></el-option>
               </el-select>
@@ -77,11 +77,7 @@
           </el-col>
           <el-col :span="6">
             <el-form-item label="日期" prop="create_date">
-              <el-date-picker
-                v-model="form.create_date"
-                type="date"
-                placeholder="选择日期"
-              ></el-date-picker>
+              <el-date-picker v-model="form.create_date" type="date" placeholder="选择日期"></el-date-picker>
             </el-form-item>
           </el-col>
         </el-row>
@@ -102,9 +98,11 @@
     <el-card class="table">
       <el-table :data="tableList" border>
         <el-table-column label="序号" width="70px">
-          <template v-slot="scope">{{
+          <template v-slot="scope">
+            {{
             (page - 1) * size + scope.$index + 1
-          }}</template>
+            }}
+          </template>
         </el-table-column>
         <el-table-column label="题目" width="250px">
           <template v-slot="scope">
@@ -113,42 +111,33 @@
         </el-table-column>
         <el-table-column label="学科.阶段" width="180px">
           <template v-slot="scope">
-            <div>
-              {{ scope.row.subject_name }}.{{ stepObj[scope.row.step] }}
-            </div>
+            <div>{{ scope.row.subject_name }}.{{ stepObj[scope.row.step-1] }}</div>
           </template>
         </el-table-column>
         <el-table-column label="题型" width="120px">
-          <template v-slot="scope">{{
-            difficultyArr[scope.row.difficulty]
-          }}</template>
+          <template v-slot="scope">
+            {{
+            typeArr[scope.row.type-1]
+            }}
+          </template>
         </el-table-column>
-        <el-table-column
-          label="企业"
-          prop="enterprise_name"
-          width="120px"
-        ></el-table-column>
-        <el-table-column
-          label="创建者"
-          prop="username"
-          width="120px"
-        ></el-table-column>
-        <el-table-column label="状态" width="120px">
-          <template v-slot="scope">{{
+        <el-table-column label="企业" prop="enterprise_name" width="190px"></el-table-column>
+        <el-table-column label="创建者" prop="username" width="90px"></el-table-column>
+        <el-table-column label="状态" width="80px">
+          <template v-slot="scope">
+            {{
             scope.row.status == 0 ? "禁用" : "开启"
-          }}</template>
+            }}
+          </template>
         </el-table-column>
-        <el-table-column
-          label="访问量"
-          prop="reads"
-          width="120px"
-        ></el-table-column>
+        <el-table-column label="访问量" prop="reads" width="70px"></el-table-column>
         <el-table-column label="操作">
           <template v-slot="scope">
             <el-button type="text">编辑</el-button>
-            <el-button type="text" @click="update(scope.row.id)">
-              {{ scope.row.status == 0 ? "开启" : "禁用" }}
-            </el-button>
+            <el-button
+              type="text"
+              @click="update(scope.row.id)"
+            >{{ scope.row.status == 0 ? "开启" : "禁用" }}</el-button>
             <el-button type="text">删除</el-button>
           </template>
         </el-table-column>
@@ -164,27 +153,23 @@
         :total="total"
       ></el-pagination>
     </el-card>
-    <addQuestion
-      @search="search"
-      ref="addQuestion"
-      :typeArr="typeArr"
-    ></addQuestion>
+    <addQuestion @search="search" ref="addQuestion" :typeArr="typeArr"></addQuestion>
   </div>
 </template>
 
 <script>
 //导入新增题库测试组件
-import addQuestion from "@/components/addQuestion.vue"
+import addQuestion from "@/components/addQuestion.vue";
 //导入学科接口
-import { subjectList } from "@/api/subjectList.js"
+import { subjectList } from "@/api/subjectList.js";
 //导入企业接口
-import { getList } from "@/api/enterpriseList/enterpriseList.js"
+import { getList } from "@/api/enterpriseList/enterpriseList.js";
 //导入题库列表接口、设置状态接口
-import { questionList, setStatus } from "@/api/question/question.js"
+import { questionList, setStatus } from "@/api/question/question.js";
 export default {
   //注册
   components: {
-    addQuestion,
+    addQuestion
   },
   data() {
     return {
@@ -217,87 +202,88 @@ export default {
         difficulty: "", //	否	int	题目难度: 1(简单),2(一般),3(困难)
         create_date: "", //	否	string	创建日期
         page: "", //	否	string	页码 默认为1
-        limit: "", //	否	string	页尺寸 默认为10
-      },
-    }
+        limit: "" //	否	string	页尺寸 默认为10
+      }
+    };
   },
   methods: {
     //页容量改变就执行的函数
     sizeChange(size) {
       // window.console.log(size)
       //获取当前页容量
-      this.size = size
+      this.size = size;
       //默认获取第一页数据
-      this.page = 1
-      this.getData()
+      this.page = 1;
+      this.getData();
     },
     //页码改变就执行的函数
     currentChange(page) {
       // window.console.log(page)
       //获取当前页码的数据
-      this.page = page
-      this.getData()
+      this.page = page;
+      this.getData();
     },
     //获取题库列表
     getData() {
+      window.console.log(this.form);
       let data = {
         ...this.form,
         page: this.page,
-        limit: this.size,
-      }
-      questionList(data).then((res) => {
+        limit: this.size
+      };
+      questionList(data).then(res => {
         // window.console.log(res)
         //保存题库信息
-        this.tableList = res.data.items
+        this.tableList = res.data.items;
         //保存总条数
-        this.total = res.data.pagination.total
-      })
+        this.total = res.data.pagination.total;
+      });
     },
     //搜索功能
     search() {
       //默认第一页数据
-      this.page = 1
-      this.getData()
+      this.page = 1;
+      this.getData();
     },
     //清除功能
     reset() {
       //调用form的resetFields()清空表单和表单验证的方法
-      this.$refs.form.resetFields()
+      this.$refs.form.resetFields();
       //默认搜索第一页的数据
-      this.search()
+      this.search();
     },
     //设置状态功能
     update(id) {
       setStatus({ id }).then(() => {
         //提示用户
-        this.$message.success("设置状态成功")
+        this.$message.success("设置状态成功");
         //刷新数据
-        this.getData()
-      })
+        this.getData();
+      });
     },
     //新增功能
     add() {
       //打开对话框
-      this.$refs.addQuestion.isShow = true
-    },
+      this.$refs.addQuestion.isShow = true;
+    }
   },
   created() {
     //获取学科列表
-    subjectList().then((res) => {
+    subjectList().then(res => {
       // window.console.log(res)
       //保存学科信息
-      this.subject = res.data.items
-    })
+      this.subject = res.data.items;
+    });
     //获取企业列表
-    getList().then((res) => {
+    getList().then(res => {
       // window.console.log(res)
       //保存企业信息
-      this.enterpriseList = res.data.items
-    })
+      this.enterpriseList = res.data.items;
+    });
     //获取题库列表
-    this.getData()
-  },
-}
+    this.getData();
+  }
+};
 </script>
 
 <style lang="less">
